055b0f2c932b657eecdb053ce19be7e753d854d8
Catherine AUBLE <catherine-auble@orange.fr>
2020-12-02 08:48:16 +0100

    TODO 1 : - Correction de la connexion à la base (problème de timezone) - Revue du calcul de la durée du stationnement pour gérer différemment les moins d'une heure et au délà d'une heure y compris sur plusieurs jours.

--- src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
+++ src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
@@ -1,21 +1,57 @@
 package com.parkit.parkingsystem.service;
 
+import java.text.DateFormat;
+import java.text.DecimalFormat;
+import java.text.SimpleDateFormat;
+import java.time.Duration;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.Date;
+
 import com.parkit.parkingsystem.constants.Fare;
 import com.parkit.parkingsystem.model.Ticket;
 
 public class FareCalculatorService {
 
-    public void calculateFare(Ticket ticket){
+	public void calculateFare(Ticket ticket){
         if( (ticket.getOutTime() == null) || (ticket.getOutTime().before(ticket.getInTime())) ){
             throw new IllegalArgumentException("Out time provided is incorrect:"+ticket.getOutTime().toString());
         }
 
-        int inHour = ticket.getInTime().getHours();
-        int outHour = ticket.getOutTime().getHours();
+        double duration;
+       
+        Date debStationnement = ticket.getInTime();
+        Instant instantdeb = debStationnement.toInstant();
+        ZonedDateTime zonedeb = instantdeb.atZone(ZoneId.systemDefault());
+        LocalDateTime localDateDeb = zonedeb.toLocalDateTime();
+        
+        Date finStationnement =  ticket.getOutTime();
+        Instant instantfin = finStationnement.toInstant();
+        ZonedDateTime zonefin = instantfin.atZone(ZoneId.systemDefault());
+        LocalDateTime localDateFin = zonefin.toLocalDateTime();
+        
+        //long finStationnement = ticket.getOutTime();
+       
+        long duree = Duration.between(localDateDeb,localDateFin).getSeconds();
+        
+        System.out.println(duree);
+        
+        // Duree exprimée en secondes 
+        duration = (double)(StrictMath.round( duree / 60));
+        duration = duration / 60;
+        
+        if ( duree > 3599 ) {
+        	duration = StrictMath.round(duree / 3600);
+        	System.out.println(duration);
+        }
+        
+        
+        System.out.println(duration);
 
-        //TODO: Some tests are failing here. Need to check if this logic is correct
-        int duration = outHour - inHour;
-
+        
         switch (ticket.getParkingSpot().getParkingType()){
             case CAR: {
                 ticket.setPrice(duration * Fare.CAR_RATE_PER_HOUR);
--- src/test/java/com/parkit/parkingsystem/integration/ParkingDataBaseIT.java
+++ src/test/java/com/parkit/parkingsystem/integration/ParkingDataBaseIT.java
@@ -1,11 +1,7 @@
 package com.parkit.parkingsystem.integration;
 
-import com.parkit.parkingsystem.dao.ParkingSpotDAO;
-import com.parkit.parkingsystem.dao.TicketDAO;
-import com.parkit.parkingsystem.integration.config.DataBaseTestConfig;
-import com.parkit.parkingsystem.integration.service.DataBasePrepareService;
-import com.parkit.parkingsystem.service.ParkingService;
-import com.parkit.parkingsystem.util.InputReaderUtil;
+import static org.mockito.Mockito.when;
+
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -13,7 +9,13 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import static org.mockito.Mockito.when;
+
+import com.parkit.parkingsystem.dao.ParkingSpotDAO;
+import com.parkit.parkingsystem.dao.TicketDAO;
+import com.parkit.parkingsystem.integration.config.DataBaseTestConfig;
+import com.parkit.parkingsystem.integration.service.DataBasePrepareService;
+import com.parkit.parkingsystem.service.ParkingService;
+import com.parkit.parkingsystem.util.InputReaderUtil;
 
 @ExtendWith(MockitoExtension.class)
 public class ParkingDataBaseIT {
@@ -44,7 +46,6 @@ public class ParkingDataBaseIT {
 
     @AfterAll
     private static void tearDown(){
-
     }
 
     @Test
@@ -52,6 +53,7 @@ public class ParkingDataBaseIT {
         ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
         parkingService.processIncomingVehicle();
         //TODO: check that a ticket is actualy saved in DB and Parking table is updated with availability
+        
     }
 
     @Test
@@ -60,6 +62,7 @@ public class ParkingDataBaseIT {
         ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
         parkingService.processExitingVehicle();
         //TODO: check that the fare generated and out time are populated correctly in the database
+        
     }
 
 }
